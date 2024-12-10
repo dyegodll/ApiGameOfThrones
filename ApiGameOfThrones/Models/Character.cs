1c2118ac102e8d42d18cbf2016d3364ae6282e07
Dyego Lima <dyegodll18@gmail.com>
2024-12-10 15:44:39 -0300

    first commit

---
+++ ApiGameOfThrones/Models/Character.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace ApiGameOfThrones.Models;
+
+internal class Character
+{
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+    [JsonPropertyName("gender")]
+    public string? Gender { get; set; }
+    [JsonPropertyName("born")]
+    public string? Born { get; set; }
+    [JsonPropertyName("title")]
+    public List<string>? Title { get; set; }
+    [JsonPropertyName("aliases")]
+    public List<string>? Aliases { get; set; }
+
+    public void InfoCharacter()
+    {
+        Console.WriteLine($"☻ {Name} - {Url} ☻");
+        
+        if(Born != null && !Born.Equals("")) Console.WriteLine($"\t♦ Born: {Born}");
+        
+        if(Gender != null) Console.WriteLine($"\t♦ Gender: {Gender}");
+
+        if (Title != null && Title.Count > 0) Console.WriteLine($"\t♦ Titles: {string.Join(", ", Title!)}");
+
+        if (Aliases  != null && Aliases.Count > 0) Console.WriteLine($"\t♦ Aliases: {string.Join(", ", Aliases!)}");
+
+        Console.WriteLine();
+    }
+}
---
+++ ApiGameOfThrones/Program.cs
@@ -0,0 +1,24 @@
+﻿using ApiGameOfThrones.Models;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        for(int i = 1; i < 100; i++)
+        {
+            string json = await client.GetStringAsync($"https://www.anapioficeandfire.com/api/characters/{i}");
+            var obj = JsonSerializer.Deserialize<Character>(json)!;
+
+            if( obj.Name != null && !obj.Name.Equals("") )
+            {
+                obj.InfoCharacter();
+            }
+        }
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
